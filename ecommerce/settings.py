1deb1bf704d880577220726756521a37d1d47b74
prakriti <prakriti673@gmail.com>
2024-01-14 20:35:57 +0530

    favicon error2

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -130,10 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+#Added manually
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'static'),
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
